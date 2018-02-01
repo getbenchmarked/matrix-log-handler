562b2a597640a91816214c41f1091e6f2373a802
Gergely Polonkai <gergely@polonkai.eu>
2018-02-01 12:34:48 +0100

    Add a class docstring

--- matrix_log_handler/__init__.py
+++ matrix_log_handler/__init__.py
@@ -7,6 +7,25 @@ from matrix_client.api import MatrixHttpApi
 
 
 class MatrixLogHandler(logging.Handler):
+    """Log handler to send records to a Matrix room.
+
+    :param base_url: the base URL of the Matrix homeserver to use.  This should not include the
+        `_matrix/client/` part.
+    :param room_id: the room ID to send the messages to.  The account must be in the room and must
+        have the sufficient power level to send messages.  Note that this must be the room ID
+        (!foo:bar.org), not an alias!
+    :param username: a username to use for logging in if no access token was set
+    :param password: a password to use for logging in if no access token was set
+    :param token: a valid access token that can be used to identify the log handler to the Matrix
+        homeserver.  This is the recommended way for authorization.  If not set, a
+        username/password pair must be set so the handler can acquire an access token.
+    :type base_url: str
+    :type room_id: str
+    :type username: str
+    :type password: str
+    :type token: str
+    """
+
     def __init__(self, base_url, room_id,
                username=None, password=None, token=None, fail_silent=False,
                format='[%(levelname)s] [%(asctime)s] [%(name)s] - %(message)s'):
